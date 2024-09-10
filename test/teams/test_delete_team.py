b0bb9d699c2f6db342f57f66c984aa1871c3eb1a
darwing-21 <darwingarcia2130@gmail.com>
2024-09-10 00:16:43 -0400

    fix(delete_team): teardown correction

--- test/teams/test_delete_team.py
+++ test/teams/test_delete_team.py
@@ -21,18 +21,22 @@ def test_delete_team_with_non_existent_id_returns_404():
 
 
 @pytest.mark.deleteteam
-def test_unauthenticated_user_cannot_delete_team_returns_401(setup_team):
+def test_unauthenticated_user_cannot_delete_team_returns_401(setup_team, teardown_team):
     team = setup_team
+    created_teams = teardown_team
     url = EndpointTeams.get_team_id(team['id'])
     response = TeamService.delete_team(url, "invalid_user")
+    created_teams.append(team)
     AssertionManager.assert_status_code_401(response)
 
 
 @pytest.mark.deleteteam
-def test_user_without_permissions_cannot_delete_team_returns_403(setup_team):
+def test_user_without_permissions_cannot_delete_team_returns_403(setup_team, teardown_team):
     team = setup_team
+    created_teams = teardown_team
     url = EndpointTeams.get_team_id(team['id'])
     response = TeamService.delete_team(url, "no_team_access_user")
+    created_teams.append(team)
     AssertionManager.assert_status_code_403(response)
 
 
