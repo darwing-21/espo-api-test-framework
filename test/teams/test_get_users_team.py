bcdb5a185246978ced1d2cb865aeb61489efa94f
darwing-21 <darwingarcia2130@gmail.com>
2024-09-19 02:36:50 -0400

    feat(list_user_team): update test case

--- test/teams/test_get_users_team.py
+++ test/teams/test_get_users_team.py
@@ -34,55 +34,6 @@ def test_filter_active_users_using_primary_filter_returns_200(setup_teardown_use
 
 
 @pytest.mark.listuserteam
-def test_select_specific_fields_in_response_using_select_parameter_returns_200(setup_teardown_user_team):
-    team = setup_teardown_user_team
-    url = EndpointTeams.get_list_users_team(team['id'], select="name")
-    response = TeamService.get_list_users_team(url)
-    AssertionManager.assert_status_code_200(response)
-    AssertionManager.assert_team_users_schema_file(response)
-    AssertionManager.assert_field_in_list(response, "name")
-
-
-@pytest.mark.listuserteam
-def test_handle_min_max_size_parameter_returns_200(setup_teardown_user_team):
-    team = setup_teardown_user_team
-    url = EndpointTeams.get_list_users_team(team['id'], maxSize=1)
-    response = TeamService.get_list_users_team(url)
-    AssertionManager.assert_status_code_200(response)
-    AssertionManager.assert_team_users_schema_file(response)
-    AssertionManager.assert_list_size(response, 1)
-
-
-@pytest.mark.listuserteam
-def test_handle_max_max_size_parameter_returns_200(setup_teardown_user_team):
-    team = setup_teardown_user_team
-    url = EndpointTeams.get_list_users_team(team['id'], maxSize=1)
-    response = TeamService.get_list_users_team(url)
-    AssertionManager.assert_status_code_200(response)
-    AssertionManager.assert_team_users_schema_file(response)
-    AssertionManager.assert_total_greater_than_zero(response)
-
-
-@pytest.mark.listuserteam
-def test_reject_max_size_above_limit_returns_403(setup_teardown_user_team):
-    team = setup_teardown_user_team
-    url = EndpointTeams.get_list_users_team(team['id'], maxSize=201)
-    response = TeamService.get_list_users_team(url)
-    AssertionManager.assert_status_code_403(response)
-
-
-@pytest.mark.listuserteam
-def test_handle_min_offset_parameter_returns_200(setup_teardown_user_team):
-    team = setup_teardown_user_team
-    url = EndpointTeams.get_list_users_team(team['id'], offset=0)
-    response = TeamService.get_list_users_team(url)
-    AssertionManager.assert_status_code_200(response)
-    AssertionManager.assert_team_users_schema_file(response)
-    AssertionManager.assert_list_not_empty(response)
-    AssertionManager.assert_total_greater_than_zero(response)
-
-
-@pytest.mark.listuserteam
 @pytest.mark.xfail(reason="This test case is expected to fail due to known issue.", condition=True)
 def test_reject_negative_offset_value_returns_400(setup_teardown_user_team):
     team = setup_teardown_user_team
@@ -110,46 +61,6 @@ def test_order_results_using_order_by_parameter_returns_200(setup_teardown_user_
     AssertionManager.assert_list_not_empty(response)
     AssertionManager.assert_total_greater_than_zero(response)
     AssertionManager.assert_list_order_ascending(response, "name")
-
-
-@pytest.mark.listuserteam
-def test_reject_invalid_values_in_order_parameter_returns_400(setup_teardown_user_team):
-    team = setup_teardown_user_team
-    url = EndpointTeams.get_list_users_team(team['id'], orderBy="unknown")
-    response = TeamService.get_list_users_team(url)
-    AssertionManager.assert_status_code_400(response)
-
-
-@pytest.mark.listuserteam
-def test_reject_invalid_order_parameter_returns_400(setup_teardown_user_team):
-    team = setup_teardown_user_team
-    url = EndpointTeams.get_list_users_team(team['id'], order="unknown")
-    response = TeamService.get_list_users_team(url)
-    AssertionManager.assert_status_code_400(response)
-
-
-@pytest.mark.listuserteam
-def test_order_results_ascending_using_order_parameter_returns_200(setup_teardown_user_team):
-    team = setup_teardown_user_team
-    url = EndpointTeams.get_list_users_team(team['id'], order="asc")
-    response = TeamService.get_list_users_team(url)
-    AssertionManager.assert_status_code_200(response)
-    AssertionManager.assert_list_select_team_schema_file(response)
-    AssertionManager.assert_list_not_empty(response)
-    AssertionManager.assert_total_greater_than_zero(response)
-    AssertionManager.assert_list_order_ascending(response, "name")
-
-
-@pytest.mark.listuserteam
-def test_order_results_descending_using_order_parameter_returns_200(setup_teardown_user_team):
-    team = setup_teardown_user_team
-    url = EndpointTeams.get_list_users_team(team['id'], order="desc")
-    response = TeamService.get_list_users_team(url)
-    AssertionManager.assert_status_code_200(response)
-    AssertionManager.assert_list_select_team_schema_file(response)
-    AssertionManager.assert_list_not_empty(response)
-    AssertionManager.assert_total_greater_than_zero(response)
-    AssertionManager.assert_list_order_descending(response, "name")
 
 
 @pytest.mark.listuserteam
